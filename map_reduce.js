37260b012b286ff0bc4ef22ec837f1eec8b7d6f5
andrewbeng89 <madbeng@gmail.com>
2013-02-20 19:17:16 +0800

    update map_reduce module

--- map_reduce.js
+++ map_reduce.js
@@ -35,7 +35,6 @@ app.get('/users', user.list);
 var mongoose = require('mongoose'), watch = require('./models/watch'), repo = require('./models/repo'), languages = require('./models/lang');
 mongoose.connect('mongodb://test:test1234@ds039467.mongolab.com:39467/cloud_a2');
 
-console.log('\nFind the programming language that found across the most number of repositories:');
 var lang_map_options = {
 	map : function() {
 		for (var i = 0; i < this.langs.length; i++) {
@@ -52,6 +51,8 @@ var lang_map_options = {
 	verbose : true
 }
 languages.mapReduce(lang_map_options, function(err, model, stats) {
+	console.log('\nFind the programming language that found across the most number of repositories:');
+	console.log('map reduce took %d ms', stats.processtime);
 	console.log(stats);
 });
 
@@ -69,8 +70,8 @@ var options = {
 	verbose : true
 }
 
-console.log('\nFind the repo with the most number of wathcers:');
 watch.mapReduce(options, function (err, model, stats) {
+	console.log('\nFind the repo with the most number of wathcers:');	
 	console.log('map reduce took %d ms', stats.processtime);
 	var max_watchers;
 	model.aggregate({
