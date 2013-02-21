b7d4e23e706e3ad515865fffe504f5148aec0c0b
andrewbeng89 <madbeng@gmail.com>
2013-02-21 14:03:16 +0800

    Included code comments for text to JSON  loading and map_reduce.js

--- load_lang.js
+++ load_lang.js
@@ -41,21 +41,28 @@ mongoose.connect('mongodb://test:test1234@ds039467.mongolab.com:39467/cloud_a2')
 function TrimColon(text) {
   	return text.toString().replace(/^(.*?):*$/, '$1');
 }
+
+// Read the .txt file and split by line
 var array = fs.readFileSync("data/lang.txt").toString().split("\n");
 var data_length = array.length;
 
 function load_lang() {
+	// Array to contain all repository objects
 	var repo_langs = [];
 	
 	for (var i = 0; i < data_length; i++) {
 		var lineHash = TrimColon(array[i]).split(":");
+		// The repository object
 		var repo_entry = {};
 		repo_entry.repo_id = lineHash[0];
+		// The langs[] of the repository object
 		repo_entry.langs = [];
 		var langs = lineHash[1].toString().split(',');
 		for (var count = 0; count < langs.length; count++) {
 			var langHash = langs[count].toString().split(';');
+			// Push a new lang object for each language found in a repository
 			repo_entry.langs.push({_id:{$oid:new mongoose.Types.ObjectId},lang:langHash[0],lines:langHash[1]});
+			// Sort the lang objects by number of lines, descending
 			repo_entry.langs.sort(function(a, b) {
 				return b.lines - a.lines;
 			});
@@ -63,9 +70,11 @@ function load_lang() {
 		repo_langs.push(repo_entry);
     }
     console.log(repo_langs);
+    // Write JSON string to lang.json
     fs.writeFile('./data/lang.json', JSON.stringify(repo_langs), function (err) {
   		if (err) return console.log(err);
 	});
 }
 
+// Execute loading function
 load_lang();
--- map_reduce.js
+++ map_reduce.js
@@ -32,19 +32,25 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+// Import mongoose and mongoose model shcemas required for the exercise
 var mongoose = require('mongoose'), watch = require('./models/watch'), repo = require('./models/repo'), languages = require('./models/lang');
+// Connect mongoose to MongoLab database
 mongoose.connect('mongodb://test:test1234@ds039467.mongolab.com:39467/cloud_a2');
 
+// Declare the MapReduce options for operation on the langmodels collection in MongoLab
 var lang_map_options = {
+	// Emit language:count pair for each language in the langs[] property of the model
 	map : function() {
 		for (var i = 0; i < this.langs.length; i++) {
 			emit(this.langs[i].lang, 1);
 		}
 	},
+	// Reduce the map output by summing the repo counts for each language
 	reduce : function(k, count) {
 		var reducedValue = {lang:k, num_repos: Array.sum(count).length};
 		return reducedValue;
 	},
+	// Declare the output collection in MongoLab
 	out : {
 		replace : 'repos_per_language'
 	},
@@ -53,6 +59,7 @@ var lang_map_options = {
 languages.mapReduce(lang_map_options, function(err, model, stats) {
 	console.log('\nFind the programming language that is found \nacross the most number of repositories:');
 	console.log('map reduce took %d ms', stats.processtime);
+	// Aggregate the output collection model to find the language found in the most number of repos
 	model.aggregate({
 		$group: { _id: 'null', maxRepos: { $max: '$value.num_repos' }}
 	},
@@ -61,8 +68,8 @@ languages.mapReduce(lang_map_options, function(err, model, stats) {
 	}, 
 	function (err, res) {
   		if (err) return handleError(err);
-  		//console.log(res);
   		max_repos = res[0].maxRepos;
+  		// Query output models to display the result of the aggregation
   		model.$where('this.value.num_repos == '+max_repos).exec(function (err, docs) {
   			console.log('language: ' + docs[0]._id + ', num_repos: ' + docs[0].value.num_repos);
   			if (err) return handleError(err);
@@ -70,14 +77,18 @@ languages.mapReduce(lang_map_options, function(err, model, stats) {
 	});
 });
 
+// Declare the MapReduce options for operation on the watchmodels collection in MongoLab
 var options = {
+	// Map repo_id to a user_id watching the repo
 	map : function() {
 		emit(this.repo_id, this.user_id);
 	},
+	// Reduce the map output by summing the user_ids
 	reduce : function(k, user_id) {
 		var reducedValue = {repo_id:k, num_watchers : Array.sum(user_id).length};
            return reducedValue;
 	},
+	// Declare the output collection in MongoLab
 	out : { 
 		replace : 'watchers_per_repo'
 	},
@@ -88,6 +99,7 @@ watch.mapReduce(options, function (err, model, stats) {
 	console.log('\nFind the repo with the most number of wathcers:');	
 	console.log('map reduce took %d ms', stats.processtime);
 	var max_watchers;
+	// Aggreate the output collection model to find the repository with the most watchers
 	model.aggregate({
 		$group: { _id: 'null', maxWatchers: { $max: '$value.num_watchers' }}
 	},
@@ -99,9 +111,9 @@ watch.mapReduce(options, function (err, model, stats) {
   		max_watchers = res[0].maxWatchers;
   		console.log('most number of watchers per repo: ' + max_watchers);
   		model.$where('this.value.num_watchers == '+max_watchers).exec(function (err, docs) {
-  			//console.log(docs);
   			repo.findOne({repo_id:docs[0].value.repo_id}, function (err, doc) {
   				console.log('repo_id: ' + doc.repo_id + ', repo_name: ' + doc.repo_name);
+  				// Find the primary language present in the repository
   				languages.findOne({repo_id:doc.repo_id.toString()}, function(err, doc) {
   					if (err) return handleError(err);
   					console.log('primary programming language of repo: ' + doc.langs[0].lang);
